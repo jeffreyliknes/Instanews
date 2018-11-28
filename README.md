309d78045e0c51f8fc56910109d6131eee69f9f9
Jeff Liknes <jeff.liknes@gmail.com>
2018-11-28 15:39:12 -0800

    Moved README to the root directory, and made a slight change to the layout

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 
 Instanews API assignment 
 
+About: 
+    Instanews is a website that displays news articles with a selection of different fliters to choose from.
+
 This is assignment is a focus on using AJAX to get news articles and using JQuery to properly display them.
 
 API was created using top stories from New York Times "https://api.nytimes.com/svc/topstories/v2/".
@@ -12,6 +15,9 @@ as well as SCSS and HTML. Starting with mobile-first then using responsive layou
 
 
 I used GULP to help find errors by linting and reloading browsers.
+
+Learned:
+    How to use APIs to retrieve news articles and display them without reloading the page.
 
 
 Created by Jeff Liknes.
--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
 $(document).ready(function() {
+  const $loading = $(".loading");
   $("#select").on("change", function() {
-    // loading gif
-    $(".loading").show();
     let select = $("#select").val();
+    $loading.show();
+    getStories(select);
+    
+  });
+
+  function getStories(select) {
     // API from New York Times website
     $.ajax({
       method: "get",
-      url:
-        "https://api.nytimes.com/svc/topstories/v2/" +
-        select +
-        ".json?api-key=284e1a4e58644ff8b5c13eb355b79961"
-    }).done(function(data) {
-      $(".results").empty();
-      $(".container").addClass("container-height");
-      $(".logo-container").addClass("styling-container");
-      $(".logo").addClass("header-styling");
+      url: `https://api.nytimes.com/svc/topstories/v2/${select}.json?api-key=284e1a4e58644ff8b5c13eb355b79961`
+    })
+      .done(function(data) {
+        // loading gif
 
-      let nytData = data.results
-        .filter(function(item) {
-          return item.multimedia.length;
-        })
-        .slice(0, 12);
+        $(".results").empty();
+        $(".loading").show();
+        $(".container").addClass("container-height");
+        $(".logo-container").addClass("styling-container");
+        $(".logo").addClass("header-styling");
 
-      $.each(nytData, function(index, value) {
-        console.log(value);
-        $(".results").append(
-          `<div class="article-wrapper">
+        let nytData = data.results
+          .filter(function(item) {
+            return item.multimedia.length;
+          })
+          .slice(0, 12);
+
+        for (let value of nytData) {
+          
+          $(".results").append(
+            `<div class="article-wrapper">
            <a href="${value.url}">
            <div class="article-abstract-wrapper">
            <div class="article" style="background:url(${
@@ -36,11 +42,15 @@ $(document).ready(function() {
            }</p></div></div>
         </div>
         </div></a>`
-        );
-      }).fail(function(err) {
+          );
+        }
+      })
+      .fail(function(err) {
         throw err;
+      })
+      .always(function() {
+        
+        $loading.hide();
       });
-    });
-    $(".loading").remove();
-  });
+  }
 });
